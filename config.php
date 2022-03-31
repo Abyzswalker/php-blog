0570194cfc28bd003ccb32d0cedb987df79a57b6
Oleg Fil <abyzswalker@gmail.com>
2022-03-31 12:01:10 +0300

    .

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'title' => 'Blog PHP',
+    'instagram' => 'https://www.instagram.com/abyzswalker',
+    'db' => [
+        'server' => 'db',
+        'username' => 'dbadmin',
+        'password' => '123456',
+        'name' => 'blog-php'
+    ]
+];
